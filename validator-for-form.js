91dc3e9e83666b8027e07a513d13f8122d526399
Elkanah <elkanahawesome@gmail.com>
2022-07-17 23:15:00 -0500

    added something cool

--- validator-for-form.js
+++ validator-for-form.js
@@ -11,15 +11,26 @@ const emailRegex =
 const form = document.getElementById("form");
 const submitButton = document.querySelector(".create-acc");
 
-form.addEventListener("submit", (event) => {
-    nameValidation(event);
-    emailValidation(event);
-    passwordValidation(event);
-});
+let truthy_or_falsy_like_a_parsly = [];
+
+// for when user submits data to server
+// --------------------------------------------------------------
+/* form.addEventListener("submit", (event) => {
+        truthy_or_falsy_like_a_parsly = true;
+        nameValidation(event);
+        emailValidation(event);
+        passwordValidation(event);
+
+    }); */
+// --------------------------------------------------------------
+
+// top addeventlistener will not work if this one is active
+// for when user clicks on button
 submitButton.addEventListener("click", (event) => {
     nameValidation(event);
     emailValidation(event);
     passwordValidation(event);
+    secretStuff(truthy_or_falsy_like_a_parsly, event);
 });
 
 /* makes sure that first and last name is not empty */
@@ -28,14 +39,15 @@ function nameValidation(event) {
     const lastNameValue = lastName.value.trim();
 
     if (firstNameValue === "") {
-        firstName.classList.add("error");
-
+        errorCssStyling();
         event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
     }
 
     if (lastNameValue === "") {
-        lastName.classList.add("error");
+        errorCssStyling();
         event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
     }
 }
 
@@ -44,20 +56,73 @@ function emailValidation(event) {
 
     // make sure email is not empty
     if (emailValue === "") {
-        email.classList.add("error");
+        errorCssStyling();
         event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
     }
 
     // then check if email is a valid format
     else if (!emailRegex.test(emailValue)) {
+        errorCssStyling();
         email.classList.add("error", "invalidEmailAddress");
         event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
+        truthy_or_falsy_like_a_parsly = [];
     }
 }
+
+function passwordValidation(event) {
+    const passwordValue = password.value.trim();
+    const confirmPasswordValue = confirmPassword.value.trim();
+
+    // make sure passwords are not empty
+    if (passwordValue === "") {
+        errorCssStyling();
+        event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
+        return;
+    }
+
+    if (confirmPasswordValue === "") {
+        errorCssStyling();
+        confirmPassword.classList.add("error", "matchingPasswordsFalse");
+        event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
+        return;
+    }
+
+    // if passwords do not match error
+    if (passwordValue !== confirmPasswordValue) {
+        errorCssStyling();
+        confirmPassword.classList.add("error", "matchingPasswordsFalse");
+        event.preventDefault();
+        truthy_or_falsy_like_a_parsly.push(false);
+        return;
+    }
+
+    confirmPassword.classList.remove("matchingPasswordsFalse");
+}
+
+// Phone number validation do this thing bruh https://github.com/googlei18n/libphonenumber
 
 function errorCssStyling() {
     const requiredInputs = document.querySelectorAll("input[required='true']");
     requiredInputs.forEach((input) => {
         input.classList.add("error");
     });
+}
+
+function secretStuff(lawlDud, event) {
+    console.log("deeznuttzs");
+
+    if (!lawlDud.includes(false)) {
+        event.preventDefault();
+        location.replace("https://youtu.be/T14DQkV0fEQ");
+
+        /*  window.location.replace("https://youtu.be/T14DQkV0fEQ");
+
+        window.location.href = "https://youtu.be/T14DQkV0fEQ"; */
+    }
+    truthy_or_falsy_like_a_parsly = [];
+    console.log("the end");
 }
